05316412b0664b587de0ca91ad18f5c2bf2fc643
boogi590 <simonfraiberg@gmail.com>
2024-11-29 19:09:36 +0200

    added dogs

--- landing_page/src/components/animation/programmer/ProgrammerAnimation.jsx
+++ landing_page/src/components/animation/programmer/ProgrammerAnimation.jsx
@@ -15,23 +15,51 @@ const ProgrammerAnimation = () => {
 
   return (
     <>
-      <div style={{ display: "flex", alignItems: "center" }}>
+      <Lottie
+        lottieRef={lottieRef}
+        animationData={programmer}
+        loop={false} // Disable looping to play once
+        autoplay={true} // Start animation on load
+        style={{
+          height: "auto",
+          margin: "5vw",
+          marginBottom: "0",
+          padding: 10,
+          paddingBottom: "0",
+          position: "relative",
+        }}
+        rendererSettings={{
+          preserveAspectRatio: "xMidYMid slice", // Optional: preserves aspect ratio
+          clearCanvas: true, // Improve performance
+        }}
+      />
+      <div
+        style={{ display: "flex", alignItems: "center", position: "absolute" }}
+      >
         <Lottie
-          lottieRef={lottieRef}
-          animationData={programmer}
-          loop={false} // Disable looping to play once
-          autoplay={true} // Start animation on load
-          style={{ width: 600, height: 600, padding: 10, marginTop: 100 }}
-          rendererSettings={{
-            preserveAspectRatio: "xMidYMid slice", // Optional: preserves aspect ratio
-            clearCanvas: true, // Improve performance
+          loop
+          animationData={dog}
+          play={1}
+          style={{
+            width: "15%", // Adjust width as needed
           }}
         />
         <Lottie
           loop
           animationData={dog}
           play={1}
-          style={{ width: "10vw", bottom: "0" }}
+          style={{
+            width: "15%", // Adjust width as needed
+          }}
+        />
+        <Lottie
+          loop
+          animationData={dog}
+          play={1}
+          style={{
+            width: "20%", // Adjust width as needed
+            paddingBottom: "20px",
+          }}
         />
       </div>
     </>
