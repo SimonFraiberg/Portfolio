9d402a150fa36fe237081e3b13c2e26894e30a05
boogi590 <simonfraiberg@gmail.com>
2024-12-28 11:11:28 +0200

    made animation limited to one time

--- landing_page/src/pages/HomePage.jsx
+++ landing_page/src/pages/HomePage.jsx
@@ -39,8 +39,6 @@ export default function HomePage() {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
             entry.target.classList.add("visible");
-          } else {
-            entry.target.classList.remove("visible");
           }
         });
       },
